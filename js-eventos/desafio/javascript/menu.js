86d22f7f12a7e54c0a865ca5c3e7892161bf4305
Vinicius Sousa <viniciusxsousa@outlook.com>
2022-08-18 21:29:24 -0300

    Tratamento para falta de JS

--- js-eventos/desafio/javascript/menu.js
+++ js-eventos/desafio/javascript/menu.js
@@ -3,6 +3,9 @@
 
     var $hamburguerMenu = document.querySelector('.header-nav__hamburgger');
     var $html = document.querySelector('html');
+
+    $html.classList.remove('no-js');
+    $html.classList.add('js');
     
     $hamburguerMenu.addEventListener('click', function(){
         $html.classList.toggle('menu-opened');
